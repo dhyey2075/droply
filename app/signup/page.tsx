e22765088b903497ef96a1116e6f9963d8ddcf52
Dhyey Parekh <dhyeyhparekh@gmail.com>
2025-04-26 15:08:22 +0530

    clerk authentication done

---
+++ app/signup/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignUpForm from '@/components/SignUpForm';
+
+const Page: React.FC = () => {
+  return (
+    <div id="clerk-captcha">
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default Page;
