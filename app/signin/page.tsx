3a8b53c906f4233d718dea7f5417ab97447e6204
Dhyey Parekh <dhyeyhparekh@gmail.com>
2025-05-06 21:44:20 +0530

    refactor: simplify SignIn and SignUp forms by using Clerk components

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -3,7 +3,7 @@ import SignInForm from '@/components/SignInForm';
 
 const Page: React.FC = () => {
   return (
-    <div id="clerk-captcha">
+    <div id="clerk-captcha" className='flex flex-col items-center justify-center h-screen'>
       <SignInForm />
     </div>
   );
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -3,7 +3,7 @@ import SignUpForm from '@/components/SignUpForm';
 
 const Page: React.FC = () => {
   return (
-    <div id="clerk-captcha">
+    <div id="clerk-captcha" className='flex flex-col items-center justify-center h-screen'>
       <SignUpForm />
     </div>
   );
