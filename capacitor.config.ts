b40bf724016f9a5e4eda590a58fd84749c82f95e
Dhyey <dhyeyhparekh@gmail.com>
2026-01-30 15:47:36 +0530

    feat: update Next.js configuration for mobile builds
    - Set output to 'export' for better compatibility with Capacitor.
    - Disabled image optimization and trailing slashes to enhance mobile performance.

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yourname.droply',
+  appName: 'Droply',
+  webDir: 'public',
+
+  server: {
+    url: 'https://droply.dhyeyparekh.in',
+  },
+};
+
+export default config;
--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // Disable image optimization for Capacitor builds
+  output: 'export',
   images: {
     unoptimized: true,
   },
+  
   // Disable trailing slash for better mobile compatibility
   trailingSlash: false,
 };
